8873ed297aece4d121cb3f60ef3bcab60d3a1af0
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2022-09-05 22:40:51 +0100

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { inventoryActions } from "./store/inventory-slice";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import {
